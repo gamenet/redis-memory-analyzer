3ac5d834cc38155e0ba4bd53bc8694ba3f5aa281
nikolay.bondarenko <nikolay.bondarenko@syncopate.ru>
2016-02-08 16:39:27 +0300

    perf_encoding now return all encodings with persent

--- rma/helpers/formating.py
+++ rma/helpers/formating.py
@@ -3,16 +3,43 @@ from collections import Counter
 
 
 def floored_percentage(val, digits):
+    """
+    Return string of floored value with given digits after period
+
+    :param val:
+    :param digits:
+    :return:
+    """
     val *= 10 ** (digits + 2)
     return '{1:.{0}f}%'.format(digits, floor(val) / 10 ** digits)
 
 
 def pref_encoding(data):
+    """
+    Return string with unique words in list with percentage of they frequency
+
+    :param data:
+    :return str:
+    """
     encoding_counted = Counter(data)
-    prefered_encoding = max(encoding_counted, key=encoding_counted.get)
     total = sum(encoding_counted.values())
-    return "{:<7} [{:<6}]".format(prefered_encoding, floored_percentage(encoding_counted[prefered_encoding] * 1.0 / total, 1))
+    sorted_encodings = sorted(encoding_counted.items(), key=lambda t: t[1], reverse=True)
+
+    return ' / '.join(
+            ["{:<1} [{:<4}]".format(k, floored_percentage(v * 1.0 / total, 1)) for k, v in sorted_encodings])
 
 
-def total_row(source, agg):
+def make_total_row(source, agg):
+    """
+    Execute agg column based function for source columns. For example if you need `total` in table data:
+
+    Examples:
+        src = [[1,1],[1,2],[1,3]]
+        print(make_total_row(src, [sum, min]))
+        >>> [3, 1]
+
+    :param source:
+    :param agg:
+    :return:
+    """
     return [agg[index](value) if callable(agg[index]) else agg[index] for index, value in enumerate(zip(*source))]
--- tests/helpers/helpers.py
+++ tests/helpers/helpers.py
@@ -13,4 +13,8 @@ class HelpersTestCase(unittest.TestCase):
         data = [
             [1, 2, 'abc'], [4, 3, 'abc']
         ]
-        self.assertEqual(total_row(data, [sum, sum, 'test']), [5, 5, 'test'])
+        self.assertEqual(make_total_row(data, [sum, sum, 'test']), [5, 5, 'test'])
+
+    def test_pref_encoding(self):
+        self.assertEqual('c [50.0%] / a [33.3%] / b [16.6%]', pref_encoding(['a', 'a', 'b', 'c', 'c', 'c']))
+        self.assertEqual('c [60.0%] / aaa [40.0%]', pref_encoding(['aaa', 'aaa', 'c', 'c', 'c']))
