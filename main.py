a11bbf979afb2131866f941cf1be58bb30c5da58
nikolay.bondarenko <nikolay.bondarenko@syncopate.ru>
2016-02-09 18:38:12 +0300

    Prepare for pip publishing

--- main.py
+++
@@ -1,7 +0,0 @@
-import sys
-import statistics
-
-from rma.RmaApplication import RmaApplication
-
-app = RmaApplication()
-app.run({'host': '192.168.1.133', 'port': 6379, 'match': '*'})
--- rma/__init__.py
+++ rma/__init__.py
@@ -1,7 +1,7 @@
 from rma.application import RmaApplication
 from rma.redis import RmaRedis
 
-__version__ = '0.2.1'
+__version__ = '0.1.0'
 VERSION = tuple(map(int, __version__.split('.')))
 
 __all__ = ['RmaApplication']
