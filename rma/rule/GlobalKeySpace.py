1565a99440efed4032612e59718377bb69bb5fe5
nikolay.bondarenko <nikolay.bondarenko@syncopate.ru>
2016-02-09 11:55:04 +0300

    Add info memory stat

--- rma/rule/GlobalKeySpace.py
+++ rma/rule/GlobalKeySpace.py
@@ -10,11 +10,13 @@ class GlobalKeySpace:
 
     def analyze(self, keys=[]):
         total_keys = self.redis.total_keys()
+
         keys_ = [
             ["Total keys in db", total_keys],
             ["RedisDB key space overhead", dict_overhead(total_keys)]
         ]
         keys_ += [["Used `{0}`".format(key), value] for key, value in self.redis.config_get("*max-*-*").items()]
+        keys_ += [["Info `{0}`".format(key), value] for key, value in self.redis.info('memory').items()]
 
         return [
             {
