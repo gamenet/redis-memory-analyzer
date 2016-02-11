8ad51e9b05477fa8886f13c909a75d923669b8b1
nikolay.bondarenko <nikolay.bondarenko@syncopate.ru>
2016-02-11 12:58:15 +0300

    Bump changes and version

--- rma/__init__.py
+++ rma/__init__.py
@@ -1,7 +1,7 @@
 from rma.application import RmaApplication
 from rma.redis import RmaRedis
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 VERSION = tuple(map(int, __version__.split('.')))
 
 __all__ = ['RmaApplication']
