57476846fb0caea8536004fe9aea962821f19848
nikolay.bondarenko <nikolay.bondarenko@syncopate.ru>
2016-02-15 14:00:28 +0300

    Resolve #15 add columns min and max to the list statistic enhancement

--- rma/rule/List.py
+++ rma/rule/List.py
@@ -38,14 +38,21 @@ class ListAggregator(object):
     def __init__(self, all_obj, total):
         self.total_elements = total
 
-        encode_iter, sys_iter, avg_iter, value_used_iter, value_align_iter = tee(all_obj, 5)
+        encode_iter, sys_iter, avg_iter, stdev_iter, min_iter, max_iter, value_used_iter, value_align_iter = \
+            tee(all_obj, 8)
 
         self.encoding = pref_encoding([obj.encoding for obj in encode_iter], redis_encoding_id_to_str)
         self.system = sum(obj.system for obj in sys_iter)
         if total > 1:
             self.fieldAvgCount = statistics.mean(obj.count for obj in avg_iter)
+            self.fieldStdev = statistics.stdev(obj.count for obj in stdev_iter)
+            self.fieldMinCount = min((obj.count for obj in min_iter))
+            self.fieldMaxCount = max((obj.count for obj in max_iter))
         else:
             self.fieldAvgCount = min((obj.count for obj in avg_iter))
+            self.fieldStdev = 0
+            self.fieldMinCount = self.fieldAvgCount
+            self.fieldMaxCount = self.fieldAvgCount
 
         self.valueUsedBytes = sum(obj.valueUsedBytes for obj in value_used_iter)
         self.valueAlignedBytes = sum(obj.valueAlignedBytes for obj in value_align_iter)
@@ -67,7 +74,7 @@ class List(object):
 
     def analyze(self, keys):
         key_stat = {
-            'headers': ['Match', "Count", "Avg Count", "Value mem", "Real", "Ratio", "System", "Encoding", "Total"],
+            'headers': ['Match', "Count", "Avg Count", "Min Count", "Max Count", "Stdev Count", "Value mem", "Real", "Ratio", "System", "Encoding", "Total"],
             'data': []
         }
         # Undone Prefered encoding
@@ -78,6 +85,9 @@ class List(object):
                 pattern,
                 len(data),
                 agg.fieldAvgCount,
+                agg.fieldMinCount,
+                agg.fieldMaxCount,
+                agg.fieldStdev,
                 agg.valueUsedBytes,
                 agg.valueAlignedBytes,
                 agg.valueAlignedBytes / (agg.valueUsedBytes if agg.valueUsedBytes > 0 else 1),
@@ -89,7 +99,7 @@ class List(object):
             key_stat['data'].append(stat_entry)
 
         key_stat['data'].sort(key=lambda x: x[8], reverse=True)
-        key_stat['data'].append(make_total_row(key_stat['data'], ['Total:', sum, 0, sum, sum, 0, sum, '', sum]))
+        key_stat['data'].append(make_total_row(key_stat['data'], ['Total:', sum, 0, 0, 0, 0, sum, sum, 0, sum, '', sum]))
 
         return [
             "List stat",
