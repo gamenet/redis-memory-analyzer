e9ca2e9b88d552da8dbaebaec284fa5ec8fbc6e2
nikolay.bondarenko <nikolay.bondarenko@syncopate.ru>
2016-02-15 11:47:18 +0300

    Use args in setup.py  and do not use **kwargs

--- setup.py
+++ setup.py
@@ -9,40 +9,38 @@ if you have keys like 'user:100' and 'user:101' application would pick out commo
 analyze most memory distressed data in your instance.
 '''
 
-sdict = {
-    'name': 'rma',
-    'version': __version__,
-    'description': 'Utilities to profile Redis RAM usage',
-    'long_description': long_description,
-    'url': 'https://github.com/gamenet/redis-memory-analyzer',
-    'author': 'Nikolay Bondarenko',
-    'author_email': 'misterionkell@gmail.com',
-    'maintainer': 'Nikolay Bondarenko',
-    'maintainer_email': 'misterionkell@gmail.com',
-    'keywords': ['Redis', 'Memory Profiler'],
-    'license': 'MIT',
-    'install_requires': ['redis', 'tabulate', 'tqdm', 'msgpack-python'],
-    'packages': ['rma', 'rma.helpers', 'rma.reporters', 'rma.rule', 'rma.cli'],
-    'package_data': {'rma.cli': ['*.template']},
-    'test_suite': 'tests.all_tests',
-    'entry_points': {
-        'console_scripts': [
-            'rma = rma.cli.rma_cli:main',
-        ],
-    },
-    'classifiers': [
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python'
-    ],
-}
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python'
+]
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-setup(**sdict)
+setup(name='rma',
+      version=__version__,
+      description='Utilities to profile Redis RAM usage',
+      long_description=long_description,
+      url='https://github.com/gamenet/redis-memory-analyzer',
+      author='Nikolay Bondarenko',
+      author_email='misterionkell@gmail.com',
+      maintainer='Nikolay Bondarenko',
+      maintainer_email='misterionkell@gmail.com',
+      keywords=['Redis', 'Memory Profiler'],
+      license='MIT',
+      install_requires=['redis', 'tabulate', 'tqdm', 'msgpack-python'],
+      packages=['rma', 'rma.helpers', 'rma.reporters', 'rma.rule', 'rma.cli'],
+      package_data={'rma.cli': ['*.template']},
+      test_suite='tests.all_tests',
+      entry_points={
+          'console_scripts': [
+              'rma = rma.cli.rma_cli:main',
+          ],
+      },
+      classifiers=classifiers)
